dd73be54e343681c86d0d6655f119a85e65c41b7
FynnHer <fynn-lasse.herrmann@gmx.de>
2025-12-15 18:24:53 +0100

    GPO for virtio gpu pci (wip)

--- kernel.c
+++ kernel.c
@@ -4,8 +4,14 @@
 
 
 void kernel_main() {
+
+  enable_uart();
   
   uart_puts("Hello craybond!\n");
+  uart_puts("Preparing for drawing...\n");
+  //gpu_debug();
   gpu_init();
+  //gpu_clear(0x00FF00FF);
+  uart_puts("Square drawn\n");
 
 }
