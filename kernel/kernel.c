3a15273af90e0892e5253746b68b9b242a624509
FynnHer <fynnlasse.herrmann@gmail.com>
2025-12-26 16:02:12 +0100

    Preparing fw and pci for finding dynamically

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -26,4 +26,6 @@ void kernel_main() {
     //kconsole_clear();
 
     printf("Kernel initialization finished");
+
+    printf("Now we're writing a really long string, becuase why not? Let's see how the console handles it. This should wrap around multiple lines and still be perfectly readable. If everything works as expected, we should see this entire message displayed correctly on the screen without any issues. Let's add even more text to make sure we really test the limits of our console implementation. Here we go!");
 }
--- kernel/pci.c
+++ kernel/pci.c
@@ -1,6 +1,7 @@
 #include "pci.h"
 #include "console/kio.h"
 #include "ram_e.h"
+#include "fw/fw_cfg.h"
 
 #define PCI_ECAM_BASE 0x4010000000
 #define PCI_BUS_MAX 256
@@ -9,6 +10,30 @@
 
 #define PCI_COMMAND_REGISTER 0x04
 
+static uint64_t pci_base;
+
+#define NINIT pci_base == 0x0
+
+typedef struct {
+    char signature[8];          // Expected to be "RSD PTR "
+    uint8_t checksum;           // Checksum of the first 20 bytes
+    char oem_id[6];             // OEM Identifier
+    uint8_t revision;           // Revision (0 for ACPI 1.0, 2 for ACPI 2.0+)
+    uint32_t rsdt_address;      // ACPI 1.0 addr
+    // ACPI 2.0+:
+    uint32_t length;            // Total size of RSDP (should be at least 36)
+    uint64_t xsdt_address;      // 64-bit address of XSDT (new table)
+    uint8_t extended_checksum;  // Checksum of the entire table
+    uint8_t reserved[3];
+} __attribute__((packed)) acpi_rsdp_t;
+
+void find_pci() {
+    struct fw_cfg_file file;
+    if (!fw_find_file(string_l("etc/acpi/rsdp"), &file))
+        return;
+
+    printf("Found RSDP file");
+}
 uint64_t pci_make_addr(uint32_t bus, uint32_t slot, uint32_t func, uint32_t offset){
     return PCI_ECAM_BASE
             | (((uint64_t)bus) << 20)
@@ -17,24 +42,23 @@ uint64_t pci_make_addr(uint32_t bus, uint32_t slot, uint32_t func, uint32_t offs
             | ((uint64_t)(offset & 0xFFF));
 }
 
-uint64_t pci_get_bar(uint64_t base, uint8_t offset, uint8_t index){
+uint64_t pci_get_bar_address(uint64_t base, uint8_t offset, uint8_t index){
+    if (NINIT)
+        find_pci();
     return base + offset + (index * 4);
 }
 
 void debug_read_bar(uint64_t base, uint8_t offset, uint8_t index){
-    uint64_t addr = pci_get_bar(base, offset, index);
+    uint64_t addr = pci_get_bar_address(base, offset, index);
     uint64_t val = read32(addr);
     printf("Reading@%h (%i) content: %h", addr, index, val);
 }
 
-void inspect_bars(uint64_t base, uint8_t offset) {
-    printf("Inspecting GPU BARs...");
-    for (uint32_t bar_offset = 0; bar_offset <= 0xFF; bar_offset += 1) {
-        debug_read_bar(base, offset, bar_offset);
-    }
-}
-
 uint64_t find_pci_device(uint32_t vendor_id, uint32_t device_id) {
+
+    if (NINIT)
+        find_pci();
+    
     for (uint32_t bus = 0; bus < PCI_BUS_MAX; bus++) {
         for (uint32_t slot = 0; slot < PCI_SLOT_MAX; slot++) {
             for (uint32_t func = 0; func < PCI_FUNC_MAX; func++) {
