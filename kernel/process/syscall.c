75dbea2fd76e2abc86beb0f290090a6629fa05d6
FynnHer <fynnlasse.herrmann@gmail.com>
2026-01-04 22:24:13 +0100

    WIP: Preventing process from returning while in EL1

--- kernel/process/syscall.c
+++ kernel/process/syscall.c
@@ -9,6 +9,7 @@ that is handled by the kernel.
 #include "console/kio.h"
 #include "exception_handler.h"
 #include "console/serial/uart.h"
+#include "gic.h"
 
 void sync_el0_handler_c() {
     // We nned to inspect this call is what we think it is
@@ -23,16 +24,23 @@ void sync_el0_handler_c() {
     uint64_t x8;
     asm volatile ("mov %0, x8" : "=r"(x8));
 
-    uint64_t esr, elr, far;
+    uint64_t esr;
+    asm volatile ("mrs %0, esr_el1" : "=r"(esr));
+
+    uint64_t elr;
 
     asm volatile ("mrs %0, elr_el1" : "=r"(elr));
-    asm volatile ("mrs %0, daif" : "=r"(far));
-    
+
+    uint64_t spsr;
+    asm volatile ("mrs %0, spsr_el1" : "=r"(spsr));
+
     if (x8 == 3){
         printf_args_raw(x0,x1,x2);
+        asm volatile ("msr elr_el1, %0" : : "r"(elr));
     } else {
         handle_exception("UNEXPECTED EL0 EXCEPTION");
     }
 
+    asm volatile ("msr spsr_el1, %0" : : "r"(spsr)); // Restore user's SPSR
     asm volatile ("eret");
 }
