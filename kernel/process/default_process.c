31c7ed61cf105903edec6ccb8c6c013e121a78e7
FynnHer <fynnlasse.herrmann@gmail.com>
2026-01-05 13:27:40 +0100

    Syscall works, macro still broken, process switching while in betweeen variables broken

--- kernel/process/default_process.c
+++ kernel/process/default_process.c
@@ -12,17 +12,8 @@ static uint64_t j = 1; // Counter variable for process 1
 __attribute__((section(".text.proc1"))) // Code section for process 1
 void proc_func() {
     while (1) {
-        register uint64_t x0 asm("x0") = (uint64_t)&fmt;
-        register uint64_t x1 asm("x1") = (uint64_t)&j;
-        register uint64_t x2 asm("x2") = 1;
-        register uint64_t x8 asm("x8") = 3;
-
-        asm volatile(
-            "svc #3"
-            :
-            : "r"(x0), "r"(x1), "r"(x2), "r"(x8)
-            : "memory"
-        );
+        // printf(fmt, j);
+        printf_args(fmt, (uint64_t*)&j, 1);
         j++;
     }
 }
--- shared/syscalls/syscalls.c
+++ shared/syscalls/syscalls.c
@@ -1,18 +1,1 @@
 #include "syscalls.h"
-
-void printf_args(const char *fmt, const uint64_t *args, uint32_t arg_count) {
-    /*
-    This function handles formatted output from user space by invoking the kernel's
-    kprintf_args function. It takes a format string and an array of arguments,
-    along with the count of arguments, and passes them to the kernel for printing.
-    Example usage: printf_args("Value: %h", &value, 1); would print the hexadecimal value.
-    */
-    register uint64_t x8 asm("x8") = PRINTF_SYSCALL;
-
-    asm volatile (
-        "svc #3"
-        :
-        : "r"(x8)
-        : "memory"
-    );
-}
